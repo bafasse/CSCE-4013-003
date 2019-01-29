f0485b5e382639600ba49bd5128f8e02dc503dab
Blake Fasse <blakefasse@gmail.com>
2019-01-29 09:35:05 -0600

    Finished HW1

---
+++ HW1/src/WordCount.java
@@ -0,0 +1,87 @@
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import java.io.FileWriter;
+// import java.io.FileReader;
+// import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+
+public class WordCount {
+
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    public static int wordCount = 0;
+    public static int mapCount = 0;
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      
+      // mapCount++;
+      PrintWriter wordWriter = new PrintWriter(new FileWriter("words.txt", false));
+      PrintWriter mapWriter = new PrintWriter(new FileWriter("map.txt", false));
+      mapCount++;
+      StringTokenizer itr = new StringTokenizer(value.toString());
+
+      while (itr.hasMoreTokens()) {
+        wordCount++;
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
+      wordWriter.print(wordCount);
+      mapWriter.print(mapCount);
+      
+      wordWriter.close();
+      mapWriter.close();
+
+      System.out.println("The number of words is " + wordCount);
+      System.out.println("The number of maps is " + mapCount);
+    }
+  }
+
+  public static class IntSumReducer 
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+    public void reduce(Text key, Iterable<IntWritable> values, 
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: wordcount <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    // Count how many lines are in output file
+  }
+}
