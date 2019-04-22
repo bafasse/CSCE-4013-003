56212181f41c8eb247f374381c5a77e3b126f154
Blake <blakefasse@gmail.com>
2019-04-21 19:08:13 -0500

    Uploading hw8

--- homework/hw8/spark_test/src/main/scala/SimpleApp.scala
+++ homework/hw8/spark_test/src/main/scala/SimpleApp.scala
@@ -2,42 +2,22 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.HashPartitioner
+// import org.apache.spark.sql
+// import org.apache.spark.sql._
+
 
 object SimpleApp {
 	def main(args: Array[String]) {
-		if (args.length < 1) {
-			System.err.println("Usage: SparkPageRank <file> <iter>")
-			System.exit(1)
-		}
-
-		// showWarning()
-
-		// val spark = SparkSession
-		// .builder
 		val conf = new SparkConf().setAppName("Simple Application")
-		// .getOrCreate()
-
-		val iters = if (args.length > 1) args(1).toInt else 10
 		val sc = new SparkContext(conf)
-		val lines = sc.textFile("input.txt")
-		val links = lines.map{ s =>
-		val parts = s.split("\\s+")
-		(parts(0), parts(1))
-		}.distinct().groupByKey().cache()
-		
+		val input = sc.textFile("input.txt")
+		val links = sc.parallelize(List(("MapR",List("Baidu","Blogger")),("Baidu", List("MapR")),("Blogger",List("Google","Baidu")),("Google", List("MapR")))).partitionBy(new HashPartitioner(4)).persist()
 		var ranks = links.mapValues(v => 1.0)
-
-		for (i <- 1 to iters) {
-			val contribs = links.join(ranks).values.flatMap{ case (urls, rank) =>
-			val size = urls.size
-			urls.map(url => (url, rank / size))}
-			ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
-		}
-
-		val rdd = ranks.collect()
-		rdd.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
-		// rdd.saveAsTextFile("output")
-
-		// spark.stop()
+		val contributions = links.join(ranks).flatMap { case (url, (links, rank)) => links.map(dest => (dest, rank / links.size)) }
+		val collect = contributions.collect
+		val ranks2 = contributions.reduceByKey((x, y) => x + y).mapValues(v => 0.15 + 0.85*v)
+		val rdd = ranks.collect
+		rdd.saveAsTextFile("output")
 	}
 }
